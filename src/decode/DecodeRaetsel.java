4636ffaf8bc54eab00f8c3c5e2e0e26bacc71993
Tsoyoshy <ladolph24@gmail.com>
2024-11-18 16:01:34 +0100

    initial commit

---
+++ src/decode/DecodeRaetsel.java
@@ -0,0 +1,43 @@
+package decode;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DecodeRaetsel {
+
+  public static void main(String[] args) {
+    int key = 0; // Hier Fehlenden Schlüssel eingeben
+    // Ab hier nichts mehr Anfassen!!
+    decode(key);
+  }
+
+  public static void decode(int key) {
+    ArrayList<Integer> arrToDecode = new ArrayList<>();
+    String line;
+    String text = "";
+    try {
+      BufferedReader reader =
+          new BufferedReader(new FileReader("D:\\home\\jee\\Decode\\src\\textFiles\\chiffreCode"));
+      while (null != (line = reader.readLine())) {
+        text = line;
+        for (int i = 0; i < text.toCharArray().length; i++) {
+          char a = text.toCharArray()[i];
+          arrToDecode.add(a - 48);
+
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    Decoder decoder = new Decoder(key, arrToDecode);
+
+    System.out.println(decoder.decodeNrOne());
+    System.out.println(decoder.decodeNrTwo());
+    System.out.println(decoder.decodeNrThree());
+    System.out.println(decoder.decodeNrFour());
+    System.err.println(arrToDecode);
+  }
+}
---
+++ src/decode/DecoderRaetsel.java
@@ -0,0 +1,45 @@
+package decode;
+
+import java.util.ArrayList;
+
+public class DecoderRaetsel {
+
+  //∨∨Hier nichts anfassen!∨∨
+  private int key;
+  private ArrayList<Integer> arrToDecode;
+
+  public DecoderRaetsel(int key, ArrayList<Integer> arrToDecode) {
+    this.key = key;
+    this.arrToDecode = arrToDecode;
+  }
+  //∧∧Hier nichts anfassen!∧∧
+
+  //Gedicht Nr.1
+  public long decodeNrOne() {
+
+    return arrToDecode.get(1);
+
+  }
+
+  //Gedicht Nr.2
+  public int decodeNrTwo() {
+
+    return arrToDecode.get(1);
+
+  }
+
+  //Gedicht Nr.3
+  public int decodeNrThree() {
+
+    return arrToDecode.get(1);
+
+  }
+
+  //Gedicht Nr.4
+  public int decodeNrFour() {
+
+    return arrToDecode.get(1);
+
+  }
+
+}
